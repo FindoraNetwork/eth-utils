ff3f75608b3188437d7b7481f0d9bf989cf333a6
ccmlm <fanhui.x@gmail.com>
2022-03-10 20:21:05 +0800

    add badges && CI.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+![GitHub top language](https://img.shields.io/github/languages/top/ccmlm/eth-utils)
+[![Latest Version](https://img.shields.io/crates/v/eth-utils.svg)](https://crates.io/crates/eth-utils)
+[![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/eth-utils)
+![GitHub Workflow Status](https://img.shields.io/github/workflow/status/ccmlm/eth-utils/Rust)
+[![Minimum rustc version](https://img.shields.io/badge/rustc-1.59+-lightgray.svg)](https://github.com/rust-random/rand#rust-version-requirements)
+
 # eth-utils
+
+Utils for developing ETH-compatible blockchain.
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,24 @@
 
 pub mod ecdsa_keys;
 
-#[cfg(feature = "with_evm_precompiles")]
-pub use evm_precompiles;
+#[cfg(feature = "with_precompile_utils")]
+pub use precompile_utils;
+
+#[cfg(feature = "with_common_precompiles")]
+pub use ovr_evm_precompile_blake2;
+#[cfg(feature = "with_common_precompiles")]
+pub use ovr_evm_precompile_bn128;
+#[cfg(feature = "with_common_precompiles")]
+pub use ovr_evm_precompile_curve25519;
+#[cfg(feature = "with_common_precompiles")]
+pub use ovr_evm_precompile_ed25519;
+#[cfg(feature = "with_common_precompiles")]
+pub use ovr_evm_precompile_modexp;
+#[cfg(feature = "with_common_precompiles")]
+pub use ovr_evm_precompile_sha3fips;
+#[cfg(feature = "with_common_precompiles")]
+pub use ovr_evm_precompile_simple;
+#[cfg(feature = "with_common_precompiles")]
+pub use ovr_evm_test_vector_support;
+#[cfg(feature = "with_common_precompiles")]
+pub use ovr_fp_evm;
